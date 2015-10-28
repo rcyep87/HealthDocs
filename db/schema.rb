3b23b0e28e83a0492f3e8efb591d6a3325a696aa
Ryan Yep <ryan.yep@gmail.com>
2015-10-28 11:28:51 -0500

    Remove email attr from user model to prep for devise implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028162517) do
+ActiveRecord::Schema.define(version: 20151028162745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 20151028162517) do
     t.string   "last_name",   null: false
     t.date     "dob",         null: false
     t.string   "ssn",         null: false
-    t.string   "email",       null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
