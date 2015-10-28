1d039383d2d5ab3f29e84d44966f1a91b5c1e8cf
Ryan Yep <ryan.yep@gmail.com>
2015-10-28 15:05:23 -0500

    Devise is currently not saving certain user attributes to DB when user tries to sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :middle_name, :last_name, :dob, :ssn)
+      params.require(:user).permit(:first_name, :middle_name, :last_name, :dob, :ssn, :password)
     end
 end
